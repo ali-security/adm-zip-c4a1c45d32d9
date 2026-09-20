45a8f15520153c4f6792618e08609723854d54bc
Seal <info@sealsecurity.io>
2026-09-20 04:51:13 +0000

    CVE-2026-39244

--- test/header.js
+++ test/header.js
@@ -4,6 +4,8 @@ const { expect } = require("chai");
 describe("headers", () => {
     describe("main-header", () => {
         const mainHeader = require("../headers/mainHeader");
+        const Constants = require("../util").Constants;
+        const Utils = require("../util");
         // empty zip file
         const empty = Buffer.from("504b0506000000000000000000000000000000000000", "hex");
         const readBuf = Buffer.from("504b050600000000cac0cefaed0b0000eeffc0000000", "hex");
@@ -57,6 +59,53 @@ describe("headers", () => {
 
             expect(mainh.commentLength).to.equal(5);
             expect(mainh.mainHeaderSize).to.equal(22 + 5);
+        });
+
+        it("writes zip64 end records when entry count exceeds classic zip limit", () => {
+            const mainh = new mainHeader();
+            mainh.totalEntries = 0x10000;
+            mainh.size = 123;
+            mainh.offset = 456;
+
+            const buf = mainh.toBinary();
+            const eocdOffset = Constants.ZIP64HDR + Constants.END64HDR;
+
+            expect(buf.length).to.equal(Constants.ZIP64HDR + Constants.END64HDR + Constants.ENDHDR);
+            expect(buf.readUInt32LE(0)).to.equal(Constants.ZIP64SIG);
+            expect(Utils.readBigUInt64LE(buf, Constants.ZIP64SIZE)).to.equal(Constants.ZIP64HDR - Constants.ZIP64LEAD);
+            expect(Utils.readBigUInt64LE(buf, Constants.ZIP64SUB)).to.equal(0x10000);
+            expect(Utils.readBigUInt64LE(buf, Constants.ZIP64TOT)).to.equal(0x10000);
+            expect(Utils.readBigUInt64LE(buf, Constants.ZIP64SIZB)).to.equal(123);
+            expect(Utils.readBigUInt64LE(buf, Constants.ZIP64OFF)).to.equal(456);
+
+            expect(buf.readUInt32LE(Constants.ZIP64HDR)).to.equal(Constants.END64SIG);
+            expect(Utils.readBigUInt64LE(buf, Constants.ZIP64HDR + Constants.END64OFF)).to.equal(579);
+
+            expect(buf.readUInt32LE(eocdOffset)).to.equal(Constants.ENDSIG);
+            expect(buf.readUInt16LE(eocdOffset + Constants.ENDSUB)).to.equal(Constants.EF_ZIP64_OR_16);
+            expect(buf.readUInt16LE(eocdOffset + Constants.ENDTOT)).to.equal(Constants.EF_ZIP64_OR_16);
+            expect(buf.readUInt32LE(eocdOffset + Constants.ENDSIZ)).to.equal(123);
+            expect(buf.readUInt32LE(eocdOffset + Constants.ENDOFF)).to.equal(456);
+        });
+
+        it("reads back the zip64 end of central directory record it wrote", () => {
+            const source = new mainHeader();
+            source.totalEntries = 0x10000;
+            source.size = 123;
+            source.offset = 456;
+
+            // only the zip64 end of central directory record is handed to loadFromBinary
+            const zip64Record = source.toBinary().slice(0, Constants.ZIP64HDR);
+
+            const mainh = new mainHeader();
+            mainh.loadFromBinary(zip64Record);
+
+            expect(mainh.diskEntries).to.equal(0x10000);
+            expect(mainh.totalEntries).to.equal(0x10000);
+            // central directory size, not the size of the zip64 record itself
+            expect(mainh.size).to.equal(123);
+            expect(mainh.offset).to.equal(456);
+            expect(mainh.commentLength).to.equal(0);
         });
 
         // try read empty file
---
+++ test/zip64.test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { expect } = require("chai");
+const Zip = require("../adm-zip");
+
+describe("zip64", () => {
+    it("writes and reads archives with more than 65535 entries", function () {
+        // building 65536 entries is slow on the oldest supported node versions
+        this.timeout(120000);
+
+        const entryCount = 0x10000;
+        const zip = new Zip({ noSort: true });
+
+        for (let i = 0; i < entryCount; i++) {
+            zip.addFile(`file-${i}.txt`, "");
+        }
+
+        const buffer = zip.toBuffer();
+        const readZip = new Zip(buffer);
+
+        expect(readZip.getEntries()).to.have.lengthOf(entryCount);
+    });
+});
